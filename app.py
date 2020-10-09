818ad0c3cb1a8397a3f8c7a42c4cb223d84cecf4
Irina Kim <irinahpda@gmail.com>
2020-10-10 00:14:56 +0100

    Add secret.py with mongo db connection and add file to .gitignore.

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 import pymongo
+from secret import username, password
 
 app = Flask(__name__)
 
 # Create connection variable
-conn = 'mongodb://localhost:27017'
+# conn = 'mongodb://localhost:27017'
+conn = 'mongodb+srv://username:password@ml-mongo-db.p34ii.mongodb.net/test'
+
 
 # Pass connection to the pymongo instance.
 client = pymongo.MongoClient(conn)
